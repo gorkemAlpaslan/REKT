06dd8aa483215116d53b522938a7ef4c5f74d910
gorkemAlpaslan <gorkem.alpaslan98@gmail.com>
2023-03-16 02:45:53 +0300

    performance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import "../styles/globals.sass";
 import type { AppProps } from "next/app";
+import background from "../assets/background.png";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="container">
+    <div
+      className="container"
+      style={{ backgroundImage: `url(https://i.hizliresim.com/1r28rpw.jpg)` }}
+    >
       <Component {...pageProps} />
     </div>
   );
